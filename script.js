4f8386108f18e274084bf904c19aab4ae46c7373
NABIL HARIZ <hariznabil663@gmail.com>
2025-07-22 14:53:43 +0200

    modifu missions section to add a counter

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const span=document.getElementById('counter')
-const target=parseInt(span.textContent)
-const duration=2000
-const interval = Math.floor(duration / target);
-  
-let count =0;
 
-span.textContent = count;
 
-  const counter = setInterval(() => {
-    count++;
-    span.textContent = count;
-    if (count >= target) {
-      clearInterval(counter);
-    }
-  }, interval);
+counters=[
+    {id:"counter1",target:70},
+    {id:"counter2",target:150},
+    {id:"counter3",target:500}
+]
+
+counters.forEach(({id,target}) => {
+    const span=document.getElementById(id)
+    let count=0
+    const duration=2000
+    const interval=Math.floor(duration/target)
+
+    span.textContent=0
+
+    const counter=setInterval(()=>{
+        count++;
+        span.textContent=count;
+        if(count>=target){
+            clearInterval(counter)
+        }
+    },interval)
+});
